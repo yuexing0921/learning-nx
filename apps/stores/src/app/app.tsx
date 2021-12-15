9e4d30145780948a7a7e2f4f18ee7852e296e132
yuexing09021 <yuexing09021@gmail.com>
2021-12-15 18:17:20 +0800

    nx generate @nrwl/react:lib header

--- apps/stores/src/app/app.tsx
+++ apps/stores/src/app/app.tsx
@@ -1,9 +1,15 @@
 import styles from './app.module.less';
 import NxWelcome from './nx-welcome';
 
+// nx会自动在tsconfig.base.json生成路径
+import { Header } from '@stores/header';
+// 这种也可以，但是推荐上面这种
+// import { Header } from 'libs/header/src';
+
 export function App() {
   return (
     <>
+      <Header />
       <NxWelcome title="stores" />
       <div />
     </>
